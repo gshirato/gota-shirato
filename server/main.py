b8d442c93cfc94f809cb1b1055eab326133d7f11
Gota Shirato <g8ta18@gmail.com>
2020-06-10 12:21:08 +0900

    ADD: return render_template of index.html instead of 'hello world'

--- server/main.py
+++ server/main.py
@@ -1,10 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    hello = "Hello world"
-    return hello
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
